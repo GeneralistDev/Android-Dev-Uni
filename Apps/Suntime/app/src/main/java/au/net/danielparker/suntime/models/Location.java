87155ced4a970efbabb255b7fb11838db5cc4a0f
Daniel Parker <dparker.tech@gmail.com>
2014-09-29 21:53:08 +1000

    ENHANCE: Fully wired listview and activity for assignment 5

---
+++ Apps/Suntime/app/src/main/java/au/net/danielparker/suntime/models/Location.java
@@ -0,0 +1,110 @@
+package au.net.danielparker.suntime.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.TimeZone;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+public class Location implements Parcelable {
+
+    private String name;
+    private double latitude;
+    private double longitude;
+    private TimeZone timeZone;
+
+    public static final Parcelable.Creator<Location> CREATOR
+            = new Parcelable.Creator<Location>() {
+        public Location createFromParcel(Parcel in) {
+            return new Location(in);
+        }
+
+        public Location[] newArray(int size) {
+            return new Location[size];
+        }
+    };
+
+    public Location(Parcel in) {
+        this.name = in.readString();
+        this.latitude = in.readDouble();
+        this.longitude = in.readDouble();
+        this.timeZone = (TimeZone)in.readValue(TimeZone.class.getClassLoader());
+    }
+
+    public Location(String name, double latitude, double longitude, TimeZone timeZone) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.timeZone = timeZone;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(this.name);
+        out.writeDouble(this.latitude);
+        out.writeDouble(this.longitude);
+        out.writeValue(this.timeZone);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public TimeZone getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(TimeZone timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public static ArrayList<Location> loadLocations(InputStream locationsFile){
+        CSVReader csvReader = new CSVReader(new InputStreamReader(locationsFile));
+        String[] nextLine;
+        ArrayList<Location> locations = new ArrayList<Location>();
+        try {
+            while ((nextLine = csvReader.readNext()) != null) {
+                String name = nextLine[0];
+                double latitude = Double.parseDouble(nextLine[1]);
+                double longitude = Double.parseDouble(nextLine[2]);
+                TimeZone timeZone = TimeZone.getTimeZone(nextLine[3]);
+                locations.add(new Location(name, latitude, longitude, timeZone));
+            }
+        } catch (IOException e){
+            Log.e("SUNTIME", e.getMessage());
+        }
+
+        return locations;
+    }
+}
--- Apps/Suntime/app/src/main/java/au/net/danielparker/suntime/ui/SuntimeActivity.java
+++ Apps/Suntime/app/src/main/java/au/net/danielparker/suntime/ui/SuntimeActivity.java
@@ -8,21 +8,32 @@ import java.util.TimeZone;
 import au.net.danielparker.suntime.R;
 import au.net.danielparker.suntime.calc.AstronomicalCalendar;
 import au.net.danielparker.suntime.calc.GeoLocation;
+import au.net.danielparker.suntime.models.Location;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.DatePicker;
 import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.TextView;
 
-public class Main extends Activity 
+public class SuntimeActivity extends Activity
 {
     /** Called when the activity is first created. */
+    private Location currentLocation;
+
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_suntime);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        this.currentLocation = (Location)bundle.getParcelable("location");
+
         initializeUI();
     }
 
@@ -33,15 +44,27 @@ public class Main extends Activity
 		int year = cal.get(Calendar.YEAR);
 		int month = cal.get(Calendar.MONTH);
 		int day = cal.get(Calendar.DAY_OF_MONTH);
+        TextView locationTV = (TextView)findViewById(R.id.locationTV);
 		dp.init(year,month,day,dateChangeHandler); // setup initial values and reg. handler
 		updateTime(year, month, day);
 	}
     
 	private void updateTime(int year, int monthOfYear, int dayOfMonth)
 	{
-		TimeZone tz = TimeZone.getDefault();
-		GeoLocation geolocation = new GeoLocation("Melbourne", -37.50, 145.01, tz);
-		AstronomicalCalendar ac = new AstronomicalCalendar(geolocation);
+        GeoLocation geoLocation;
+        if (currentLocation == null) {
+            TimeZone tz = TimeZone.getDefault();
+            geoLocation = new GeoLocation("Melbourne", -37.50, 145.01, tz);
+        } else {
+            geoLocation = new GeoLocation(
+                                currentLocation.getName(), currentLocation.getLatitude(),
+                                currentLocation.getLongitude(), currentLocation.getTimeZone());
+        }
+
+        TextView locationTV = (TextView)findViewById(R.id.locationTV);
+        locationTV.setText(geoLocation.getLocationName());
+
+		AstronomicalCalendar ac = new AstronomicalCalendar(geoLocation);
 		ac.getCalendar().set(year, monthOfYear, dayOfMonth);
 		Date srise = ac.getSunrise();
 		Date sset = ac.getSunset();
