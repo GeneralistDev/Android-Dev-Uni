6a33e78b923c041986b2f3059071ac5499c55633
Daniel Parker <dparker.tech@gmail.com>
2014-10-16 00:47:34 +1100

    NEW: Assignment 6 .tex file

---
+++ Apps/Suntime-Custom/app/src/main/java/au/net/danielparker/suntime/ui/AddNewLocation.java
@@ -0,0 +1,20 @@
+package au.net.danielparker.suntime.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+import au.net.danielparker.suntime.R;
+
+/**
+ * Created by danielparker on 15/10/14.
+ */
+@EActivity(R.layout.activity_add_custom)
+public class AddNewLocation extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
