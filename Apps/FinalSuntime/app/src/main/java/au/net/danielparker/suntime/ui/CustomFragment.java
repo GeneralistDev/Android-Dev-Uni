e352b31b421a6bc11e69b23d98da995d06ca1f99
Daniel Parker <dparker.tech@gmail.com>
2014-10-29 23:15:38 +1100

    NEW: Assignment 8 tex file

--- Apps/FinalSuntime/app/src/main/java/au/net/danielparker/suntime/ui/CustomFragment.java
+++ Apps/FinalSuntime/app/src/main/java/au/net/danielparker/suntime/ui/CustomFragment.java
@@ -6,17 +6,14 @@ import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.SyncStateContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 
 import org.androidannotations.annotations.EFragment;
 
